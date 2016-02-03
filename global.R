831e52485e4ca025fd69e42a691c0885a609122e
Tony Boyles <Anthony@Boyles.cc>
2016-02-03 00:11:04 -0500

    Added SPSS to the input format options

--- global.R
+++ global.R
@@ -2,5 +2,5 @@ library(shiny)
 library(shinyBS)
 library(DT)
 
-inputFormats  <- c('Best Guess', 'arff', 'csv', 'dat', 'dbf', 'dta', 'fwf', 'rds', 'sav', 'sasb7dat', 'tsv', 'txt', 'xls', 'xlsx')
+inputFormats  <- c('Best Guess', 'arff', 'csv', 'dat', 'dbf', 'dta', 'fwf', 'rds', 'sasb7dat', 'sav', 'spss', 'tsv', 'txt', 'xls', 'xlsx')
 outputFormats <- c('arff', 'csv', 'dbf', 'dta', 'rds', 'sav', 'tsv')
